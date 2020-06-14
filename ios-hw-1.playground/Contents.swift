8e94ff48e998237961a71323fa4b919611bab85b
HaiderAlboloushi180 <halbolou05@gmail.com>
2020-06-14 17:15:56 +0300

    Finished the homework

--- ios-hw-1.playground/Contents.swift
+++ ios-hw-1.playground/Contents.swift
@@ -5,12 +5,12 @@
  
  */
 
-let name = "Ahmad"
+var name = "Ahmad"
 
 name = "Salem"
 
 
-var myWeight = 60
+var myWeight = 60.0
 var FriendWeight = 80.5
 var height = 1.5
 
@@ -38,17 +38,26 @@ var bmi = myWeight * (height * height)
  */
 
 //Person 1
-var firstName1 = ""
-var lastName1 = ""
-var email1 = ""
-var phoneNumber1 = ""
-var age1 = 0
-var country1 = ""
-var area1 = ""
-var password = ""
-var isKuwaiti = false
+var firstName1 = "Haider"
+var lastName1 = "AlBoloushi"
+var email1 = "halbolou05@gmail.com"
+var phoneNumber1 = "51232437"
+var age1 = 15
+var country1 = "Kuwait"
+var area1 = "Fnaitees"
+var password = "Haider2005"
+var isKuwaiti = true
 
 //Person 2
+var firstName2 = "Abdullah"
+var lastName2 = "Ali"
+var email2 = "abdullahali2005@gmail.com"
+var phoneNumber2 = "95537337"
+var age2 = 15
+var country2 = "Kuwait"
+var area2 = "Fnaitees"
+var password2 = "Abood007"
+var isKuwaiti2 = true
 
 
 
@@ -60,12 +69,15 @@ var isKuwaiti = false
 
 
 //Person 3
-
-
-
-
-
-
+var firstName3 = "Fatimah"
+var lastName3 = "Alhashem"
+var email3 = "falhashem80@gmail.com"
+var phoneNumber3 = "50616732"
+var age3 = 39
+var country3 = "Kuwait"
+var area3 = "Fnaitees"
+var password3 = "Falh80BZH"
+var isKuwaiti3 = true
 
 
 
@@ -82,3 +94,49 @@ var isKuwaiti = false
 
  
  */
+firstName1 != firstName2
+firstName1 != firstName3
+firstName2 != firstName3
+firstName2 != firstName1
+firstName3 != firstName1
+firstName3 != firstName2
+
+lastName1 != lastName2
+lastName1 != lastName3
+lastName2 != lastName1
+lastName2 != lastName3
+lastName3 != lastName1
+lastName3 != lastName2
+
+email1 != email2
+email1 != email3
+email2 != email1
+email2 != email3
+email3 != email2
+email3 != email1
+
+phoneNumber1 != phoneNumber2
+phoneNumber1 != phoneNumber3
+phoneNumber2 != phoneNumber1
+phoneNumber2 != phoneNumber3
+phoneNumber3 != phoneNumber2
+phoneNumber3 != phoneNumber1
+
+age1 == age2
+age1 != age3
+age2 != age3
+age2 == age1
+age3 != age1
+age3 != age2
+
+area1 == area2
+area1 == area3
+area2 == area3
+
+age1 < 18
+age2 < 18
+age3 > 18
+
+age1 != 17
+age2 != 17
+age3 != 17
